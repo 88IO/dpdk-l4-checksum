ee7b24a7d36be8e0342354cf0a13f6e71bb8d961
88IO <8810aoba@gmail.com>
2023-02-27 08:03:12 +0000

    [ADD] チェックサムオフロード比較

--- l4checksum.c
+++ l4checksum.c
@@ -16,6 +16,7 @@
 #define BURST_SIZE 32
 
 #define COUNT_MAX 10000000
+#define NUM_DIFF_32 6
 
 uint64_t total_packets = 0;
 uint64_t total_time = 0;
@@ -23,7 +24,6 @@ uint64_t total_time = 0;
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
-	struct rte_eth_conf port_conf;
 	const uint16_t rx_rings = 1, tx_rings = 1;
 	uint16_t nb_rxd = RX_RING_SIZE;
 	uint16_t nb_txd = TX_RING_SIZE;
@@ -31,11 +31,17 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 	uint16_t q;
 	struct rte_eth_dev_info dev_info;
 	struct rte_eth_txconf txconf;
+	struct rte_eth_conf port_conf = {
+		.txmode = {
+			.offloads =
+				RTE_ETH_TX_OFFLOAD_IPV4_CKSUM |
+				RTE_ETH_TX_OFFLOAD_TCP_CKSUM  |
+				RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE
+		}
+	};
 
 	if (!rte_eth_dev_is_valid_port(port))
 		return -1;
-
-	memset(&port_conf, 0, sizeof(struct rte_eth_conf));
 
 	retval = rte_eth_dev_info_get(port, &dev_info);
 	if (retval != 0) {
@@ -44,9 +50,7 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 		return retval;
 	}
 
-	if (dev_info.tx_offload_capa & RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE)
-		port_conf.txmode.offloads |=
-			RTE_ETH_TX_OFFLOAD_MBUF_FAST_FREE;
+	port_conf.txmode.offloads &= dev_info.tx_offload_capa;
 
 	/* Configure the Ethernet device. */
 	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
@@ -191,7 +195,29 @@ lcore_main(void)
 
 			// l4 checksum
 			tcph->cksum = 0;
+			#if defined(L4CSUM_DIFF) || defined(L4CSUM_OFFLOAD)
+			tcph->cksum = rte_ipv4_udptcp_cksum(ipv4h, tcph);
+			#endif
 			timespec_get(&start, TIME_UTC);
+			#ifdef L4CSUM_DIFF
+			uint32_t *pos = tcph;
+			uint32_t csum32 = ~tcph->cksum;
+			for (int i = 0; i < NUM_DIFF_32; i++)
+				csum32 = csum32_sub(csum32, pos[i]);
+			for (int i = 0; i < NUM_DIFF_32; i++)
+				csum32 = csum32_add(csum32, pos[i]);	
+			tcph->cksum = ~csum32_add_fold(csum32);
+			#endif
+			#ifdef L4CSUM_OFFLOAD
+			uint32_t csum = csum32_add(
+				csum32_add(ipv4h->src_addr, ipv4h->dst_addr),
+				(ipv4h->next_proto_id << 24) + rte_cpu_to_be_16(l3_payload_len)
+			);
+			tcph->cksum = csum32_add_fold(csum);
+			m->l2_len = sizeof(struct rte_ether_hdr);
+			m->l3_len = ipv4h->ihl << 2;
+			m->ol_flags |= (RTE_MBUF_F_TX_IPV4 | RTE_MBUF_F_TX_IP_CKSUM | RTE_MBUF_F_TX_TCP_CKSUM);
+			#endif
 			#ifdef ADD32AS64
 			uint32_t *pos = tcph;
 			uint64_t csum = csum32_add(
